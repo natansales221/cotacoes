5422b643271c3852cf60fe3f91c5cbfa03410587
natansales221 <natansales221@gmail.com>
2026-08-15 19:19:34 -0300

    adding tasks

---
+++ dags/currency_pipeline.py
@@ -0,0 +1,34 @@
+import pendulum
+
+from airflow.sdk import dag, task
+
+from src.extraction.ext_currency import Extraction
+from src.utils.utilidades import logs
+
+@dag(
+    dag_id="currency_pipeline_dev",
+    schedule=None,
+    start_date=pendulum.datetime(
+        2026,
+        1,
+        1,
+        tz="America/Sao_Paulo"
+    ),
+    catchup=False,
+    tags=["dev", "currency"],
+)
+def currency_pipeline():
+
+    @task
+    def extract_currency():
+
+        arquivo_log = logs()
+
+        service = Extraction()
+
+        service.main(arquivo_log=arquivo_log)
+
+    extract_currency()
+
+
+currency_pipeline()
