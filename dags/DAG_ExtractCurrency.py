f8a825a7bdda1372b1b0f335aa8f799de7bf864d
natansales221 <natansales221@gmail.com>
2026-08-17 13:42:35 -0300

    improving tasks

--- dags/DAG_ExtractCurrency.py
+++ dags/DAG_ExtractCurrency.py
@@ -6,7 +6,7 @@ from src.extraction.ext_currency import Extraction
 from src.utils.utilidades import logs
 
 @dag(
-    dag_id="DAG - Extract",
+    dag_id="DAG_ExtractCurrency",
     schedule=None,
     start_date=pendulum.datetime(
         2026,
