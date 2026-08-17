828cdfbd3541d98fe44a8be130d5860251f7f1f7
natansales221 <natansales221@gmail.com>
2026-08-17 14:15:44 -0300

    adding second dag

---
+++ dags/DAG_LoadCurrency.py
@@ -0,0 +1,31 @@
+import pendulum
+
+from airflow.sdk import dag, task
+
+from src.load.load_currency import LoadCurrency
+
+@dag(
+    dag_id="DAG_LoadCurrency",
+    schedule=None,
+    start_date=pendulum.datetime(
+        2026,
+        1,
+        1,
+        tz="America/Sao_Paulo"
+    ),
+    catchup=False,
+    tags=["dev", "currency"],
+)
+def currency_pipeline():
+
+    @task
+    def extract_currency():
+
+        service = LoadCurrency()
+
+        service.main()
+
+    extract_currency()
+
+
+currency_pipeline()
