5bc18f4eba43819f60adf6b09e87b128a93a149c
natansales221 <natansales221@gmail.com>
2026-09-02 11:54:07 -0300

    creating ibovespa refference

---
+++ dags/DAG_ExtractIbovespa.py
@@ -0,0 +1,32 @@
+import pendulum
+
+from airflow.sdk import dag, task
+
+from src.extraction.ext_ibovespa import ExtractIbovespa
+
+
+@dag(
+    dag_id="DAG_ExtractCurrency",
+    schedule="20 6 * * 1-5", 
+    start_date=pendulum.datetime(
+        2026,
+        1,
+        1,
+        tz="America/Sao_Paulo"
+    ),
+    catchup=False,
+    tags=["dev", "extract", "ibovespa"],
+)
+def ibovespa_pipeline():
+
+    @task
+    def extract_ibovespa():
+
+        service = ExtractIbovespa()
+
+        service.main()
+
+    extract_ibovespa()
+
+
+ibovespa_pipeline()
